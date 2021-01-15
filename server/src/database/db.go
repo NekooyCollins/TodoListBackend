88fce55b59b9a4cd39fb684018a8a322ac847fb4
Zidi Chen <zidi.chen@mailbox.tu-dresden.de>
2021-01-15 17:16:56 +0100

    [dev-2] add database pack

---
+++ server/src/database/db.go
@@ -0,0 +1,71 @@
+package database
+
+import (
+	"fmt"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// TaskType receives original task data from db
+type TaskType struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Desc        string `json:"desc"`
+	Duration    int    `json:"duration"`
+	RemainTime  int    `json:"remaintime"`
+	Type        string `json:"type"`
+	IsFinish    bool   `json:"isfinish"`
+	IsGroupTask bool   `json:"isgrouptask"`
+}
+
+// UserType receives original user data from db
+type UserType struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Passwd string `json:"passwd"`
+}
+
+// UserTaskListType receives original user-task
+// list data from db
+type UserTaskListType struct {
+	UserID int `json:"userid"`
+	TaskID int `json:"taskid"`
+}
+
+// UserFriendList receives original user-friend
+// list data from DB
+type UserFriendList struct {
+	UserID   int `json:"userid"`
+	FriendID int `json:"friendid"`
+}
+
+// DBType for chain call
+type DBType struct {
+	DBConn *sql.DB
+	Error  error
+}
+
+// DBConnect function opens and returns a connection to mysql DB
+func (db *DBType) DBConnect() *DBType {
+	DBConfig, err := GetDBConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	connStr := DBConfig.DBUsername + ":" +
+		DBConfig.DBPasswd + "@tcp(" +
+		DBConfig.DBHost + ":" +
+		DBConfig.DBPort + ")/" +
+		DBConfig.DBName
+	db.DBConn, err = sql.Open("mysql", connStr)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Connection to database successful.")
+	return db
+}
+
+// DBClose close databse connection
+func (db *DBType) DBClose() {
+	db.DBConn.Close()
+}
--- server/src/handlereq.go
+++ server/src/handlereq.go
@@ -1,5 +1,7 @@
 package main
 
+import database "./database"
+
 import (
 	"fmt"
 	"log"
@@ -13,6 +15,22 @@ func handleRequests() {
 }
 
 func getUserData(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	switch r.Method {
+	case "GET":
+		rets, err := dbconn.DBConn.Query("SELECT * FROM user")
+		if err!=nil{
+			panic(err.Error())
+		}
 
+		for rets.Next(){
+			var userdata database.UserType
+			if err = rets.Scan(&userdata.ID, &userdata.Email, &userdata.Name, &userdata.Passwd);err != nil{
+				panic(err.Error())
+			}
+
+			fmt.Printf(string(userdata.ID), userdata.Name, userdata.Email)
+		}
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
 }
